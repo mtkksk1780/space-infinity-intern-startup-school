8097e9127ab243d04437bc950174e2691c81d6f5
mtkksk1780 <mtkksk1780@gmail.com>
2025-01-13 00:39:13 +0700

    Update server.py

--- backend/src/server.py
+++ backend/src/server.py
@@ -19,11 +19,13 @@ from src.routers import seed_router as seed
 from src.middlewares import auth_middleware as auth
 # from src import prisma
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../prisma/generated')))
-print("sys.path:", sys.path)
+# sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../prisma/generated')))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../prisma')))
+# print("sys.path:", sys.path)
 # print(os.listdir('/Users/masatotakakusaki/Project/Group/Tenatch/space-infinity-intern-startup-school/backend/prisma/generated'))
-from prisma import Prisma
+# from prisma import Prisma
 # from prisma.generated import Prisma
+from generated.client import Prisma
 
 load_dotenv()
 app = FastAPI()
