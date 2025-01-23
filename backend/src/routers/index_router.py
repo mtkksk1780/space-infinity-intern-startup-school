5441a59af2e3d444dde3fc080e9f595a2940f9c0
mtkksk1780 <mtkksk1780@gmail.com>
2025-01-23 15:13:31 +0700

    Update files for experiment

--- backend/src/routers/index_router.py
+++ backend/src/routers/index_router.py
@@ -1,14 +1,13 @@
 from fastapi import APIRouter
 # from src.controllers import index_controller as controller
-from src.controllers import seed_controller as controller
+from src.controllers import account_controller as controller
 
 
 router = APIRouter()
 
 # router.get("/")(controller.index)
 
-@router.post("/")
-async def register_seed():
-    is_registered =  await controller.register_seed()
-    print("index_router.py result:", is_registered)
-    return is_registered
+@router.get("/")
+async def get_account(user_id: str = "a"):
+    result = await controller.get_account(user_id = user_id)
+    return result
